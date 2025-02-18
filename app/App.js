3ca5871d475fa8a1c518716d69c36f80dd9e59d8
BKobak <bkobak01@gmail.com>
2025-02-18 17:46:12 +0000

    LogIn page base

--- app/App.js
+++ app/App.js
@@ -1,14 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { use } from 'react';
+import { TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.header}>WELCOME</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="username"
+          placeholderTextColor="#003f5c"
+          onChangeText={(username) => setUsername(username)}
+        /> 
+      </View> 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        /> 
+      </View>
+
     </View>
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +41,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  inputView: {
+    backgroundColor: '#FFC0CB',
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 50,
+    marginBottom: 40,
+    color: 'black',
   },
 });
